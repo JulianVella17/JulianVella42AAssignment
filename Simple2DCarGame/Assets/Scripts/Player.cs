b65d83165aae9f773b896b9ab6ccbf4a671eda1a
JulianVella17 <73109687+JulianVella17@users.noreply.github.com>
2020-12-26 09:46:48 +0100

    Player Moves

---
+++ Simple2DCarGame/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    //Moves player
+    private void Move()
+    {
+        //var changes its variable type
+        //depending on what I save in it
+        var deltaX = Input.GetAxis("Horizontal");
+
+        //newXPos = current x-position  + difference in x
+        var newXpos = transform.position.x + deltaX;
+        
+        //Move the Player car to newXPos
+        this.transform.position = new Vector2(newXpos, transform.position.y);
+
+    }
+}
