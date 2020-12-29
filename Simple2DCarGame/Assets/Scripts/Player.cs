1d1b94e39f18faa225f6fada0f3c352a0998696c
JulianVella17 <73109687+JulianVella17@users.noreply.github.com>
2020-12-29 11:06:38 +0100

    Move and ViewPortToWorldPoint

--- Simple2DCarGame/Assets/Scripts/Player.cs
+++ Simple2DCarGame/Assets/Scripts/Player.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    float test = 5f;
+
+    [SerializeField] float moveSpeed = 20f;
+
+    float xMin, xMax;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
+    }
+
+    //set up boundaries of camera
+    private void SetUpMoveBoundaries()
+    {
+        //get the camera from unity
+        Camera gameCamera = Camera.main;
+
+        //xMin = 0 xMax = 1
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+
     }
 
     // Update is called once per frame
@@ -18,12 +36,12 @@ public class Player : MonoBehaviour
     //Moves player
     private void Move()
     {
-        //var changes its variable type
+       //var changes its variable type
         //depending on what I save in it
-        var deltaX = Input.GetAxis("Horizontal");
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
 
         //newXPos = current x-position  + difference in x
-        var newXpos = transform.position.x + deltaX;
+        var newXpos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
         
         //Move the Player car to newXPos
         this.transform.position = new Vector2(newXpos, transform.position.y);
